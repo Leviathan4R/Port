5f6bf0233ab00c940f09147fe7a765df82a4591f
Leviathan4R <lukyruman@gmail.com>
2025-10-08 19:16:24 +0200

    Nahranie z hodiny

---
+++ Main2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+
+int main2(int argc, char const *argv[]){
+    std::cout << "Fungujem" << std::endl;
+    return 0;
+}
---
+++ Port.cpp
@@ -0,0 +1,91 @@
+
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "serialib.h"
+#include <chrono>
+#include <thread>
+#include <windows.h>
+
+
+#define SERIAL_PORT_OUT "COM4"
+int readyToRead = 0;
+
+const size_t bufSize = 55;
+static bool run = true;
+serialib serial_out;
+
+
+const char buffer[bufSize] = "Ahoj svet!, Hello World!, Hallo Welt!, Ahoj Světe!";
+char readBuffer[bufSize] = {0};
+
+int writeLoop() {
+    while(run) {
+        if (serial_out.available() < 100*bufSize) {
+            serial_out.writeBytes(buffer, bufSize);
+        }
+    }
+}
+
+int readLoop() {
+    while(run) {
+        if(serial_out.available() > bufSize) {
+            serial_out.readBytes(readBuffer, bufSize, 0, 0);
+
+            for (size_t i = 0; i < bufSize; ++i)
+            {
+                if (buffer[i] != readBuffer[i]) {
+                    run = false;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+
+    // serialib serial_out;
+    // serialib serial_in;
+
+    char errorOpening = serial_out.openDevice(SERIAL_PORT_OUT, 115200);
+    
+    if (errorOpening != 1) {
+        return -1;
+    }
+
+
+    std::cout << "Buffer: "<< buffer << std::endl;
+
+    std::thread readingThread(readLoop);
+    std::thread writingThread(writeLoop); 
+
+    /*
+    for (size_t i = 0; i < bufSize; ++i)
+    {
+        serial_out.writeChar(buffer[i]);
+
+        serial_out.readBytes(&readChar, 1, 1);
+
+        readBuffer[i] = readChar;
+
+        std::cout << readChar << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(4));
+    }*/
+
+    writingThread.join();
+    readingThread.join();
+
+    std::cout<< "Original buffer: " << buffer <<std::endl;
+    std::cout<< "Read buffer: " << readBuffer <<std::endl;
+
+    serial_out.closeDevice();
+
+    std::cout << "Program Konci" << std::endl;
+    std::cin >> readBuffer;
+    
+    return 0;
+}
